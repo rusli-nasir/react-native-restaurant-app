c819c90a5fcf42c39567fcc40d8c7e2dc0e1aa07
gunayanil <gunayanilemre@gmail.com>
2018-12-29 14:43:24 -0800

    connected redux to App

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Main from './components/Main';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configure';
+
+const store = ConfigureStore();
 
 export default class App extends React.Component {
   render() {
     return (
+      <Provider store={store}>
       <Main />
+      </Provider>
     );
   }
 }
