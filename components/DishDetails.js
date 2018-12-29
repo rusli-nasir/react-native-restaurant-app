c54a45ea914e3fc63cc0ff1bd9aaa74eff9f18fa
gunayanil <gunayanilemre@gmail.com>
2018-12-29 14:58:50 -0800

    customized for redux store

--- components/DishDetails.js
+++ components/DishDetails.js
@@ -3,6 +3,15 @@ import { View, Text, ScrollView, FlatList } from 'react-native';
 import { Card, Icon } from 'react-native-elements';
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
+import { connect } from 'react-redux';
+import { baseURL } from '../shared/baseURL';
+
+const mapStateToProps = state => {
+    return {
+        dishes: state.dishes,
+        comments: state.comments
+    }
+}
 
 function RenderDish(props) {
     const dish = props.dish;
@@ -11,7 +20,7 @@ function RenderDish(props) {
         return(
             <Card
                 featuredTitle={dish.name}
-                image={require('./images/uthappizza.png')}
+                image={{ uri: baseURL + dish.image }}
             >
                 <Text style={{margin: 10}}>
                     {dish.description}
@@ -61,8 +70,6 @@ class DishDetail extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            dishes: DISHES,
-            comments: COMMENTS,
             favorites: []
         };
     }
@@ -81,15 +88,15 @@ class DishDetail extends Component {
         // + will return the string into a number
         return(
             <ScrollView>
-                <RenderDish dish={this.state.dishes[+dishId]} 
+                <RenderDish dish={this.props.dishes.dishes[+dishId]} 
                     favorite={this.state.favorites.some(el => el === dishId)} /* Returns true or false */
                     onPress={() => this.markFavorite(dishId)}
                     />
-                <RenderComments comments={this.state.comments.filter((comment) => comment.dishId === dishId)} />
+                <RenderComments comments={this.props.comments.comments.filter((comment) => comment.dishId === dishId)} />
             </ScrollView>
             )
     }
 
 }
 
-export default DishDetail;
+export default connect(mapStateToProps)(DishDetail);
