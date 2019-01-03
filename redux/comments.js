06a086c66356c152d6d6e7599c2442bbc5a6b8d6
gunayanil <gunayanilemre@gmail.com>
2019-01-02 16:55:01 -0800

    added ADD_COMMENT for user comments

--- redux/comments.js
+++ redux/comments.js
@@ -7,6 +7,9 @@ export const comments = (state = {
     switch(action.type) {
         case ActionTypes.ADD_COMMENTS:
             return {...state, errMess: null, comments: action.payload}
+        case ActionTypes.ADD_COMMENT: 
+            var comment = action.payload;
+            return{... state, comments: state.comments.concat(comment)};
         case ActionTypes.COMMENTS_FAILED:
             return {...state, errMess: action.payload}
         default:
