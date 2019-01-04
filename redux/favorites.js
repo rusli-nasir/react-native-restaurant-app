2d578fed2eb4770b6cb253579326e0ad56943e91
gunayanil <gunayanilemre@gmail.com>
2019-01-03 21:02:42 -0800

    added delete favorite case

--- redux/favorites.js
+++ redux/favorites.js
@@ -8,6 +8,8 @@ export const favorites = (state = [], action) => {
                 return state;
             else
                 return state.concat(action.payload) // payload contains dishId
+        case ActionTypes.DELETE_FAVORITE:
+            return state.filter((favorite) => favorite !== action.payload)
         default:
             return state;
     }
