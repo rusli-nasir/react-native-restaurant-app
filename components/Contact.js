d2d823aae926b2b2ac472efca502753e29361809
gunayanil <gunayanilemre@gmail.com>
2019-01-12 17:57:13 -0800

    implemented MailComposer to send an email

--- components/Contact.js
+++ components/Contact.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import { Card } from 'react-native-elements';
+import { Card, Icon, Button } from 'react-native-elements';
 import * as  Animatable from 'react-native-animatable';
+import { MailComposer } from 'expo';
 
+class Contact extends Component {
 
-function Contact() {
+    sendMail(){
+        MailComposer.composeAsync({
+            recipients: ['gunayanilemre@gmail.com'],
+            subject: 'Enquiry',
+            body: 'Hello, '
+        })
+    }
+
+    render(){
     return(
         <View>
             <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
@@ -20,10 +30,17 @@ function Contact() {
                     Fax: +852 8765 4321
                     Email:confusion@food.net
                     </Text>
+                    <Button 
+                        title='Send Email'
+                        buttonStyle={{backgroundColor: '#512DA8'}}
+                        icon={<Icon name='envelope-o' type='font-awesome' color='#fff' />}
+                        onPress={this.sendMail}
+                        />
                 </Card>
         </Animatable.View>
         </View>
     )
+}
 }
 
 export default Contact
